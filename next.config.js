919c6dcffb2ee99030f8a18f3e1883b8bcccece1
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-07-22 02:00:00 -0300

    getting started with git and github post

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'img.freepik.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 }
